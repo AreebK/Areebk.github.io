ceb902d931aea550f9d39e9b25e987ead9189f0b
AreebK <43014295+AreebK@users.noreply.github.com>
2018-09-24 14:09:12 -0600

    DVD
    This reverts commit 9a98a2ed293131ef877caa980219b6ade0c44301.

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //scroll up and down demo
 // Areeb Khan
 // september 19th
@@ -9,10 +10,26 @@ let scalar;
 
 function preload() {
   doit = loadImage("assets/hadtodoit.PNG")
+=======
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let dvd;
+let x, y;
+let dx, dy;
+
+function preload() {
+  dvd = loadImage("assests/dvdlogo.png")
+>>>>>>> parent of 9a98a2e... So basically im monoly
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+<<<<<<< HEAD
   imageMode(CENTER);
   scalar = 1;
 }
@@ -30,4 +47,18 @@ function mouseWheel(event){
     scalar *= 0.9;
   }
   console.log(event)
+=======
+  x = width/2;
+  y = height/2;
+  dx = random(3,0);
+  dy = random(3,0);
+}
+
+function draw() {
+  background(255);
+  image(dvd,x,y);
+
+
+
+>>>>>>> parent of 9a98a2e... So basically im monoly
 }
