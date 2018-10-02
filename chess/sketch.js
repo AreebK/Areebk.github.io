5c4fea0d07af0adff21c109ebfe33869fe6a3365
AreebK <43014295+AreebK@users.noreply.github.com>
2018-10-02 13:50:27 -0600

    Chess(fixed)

--- chess/sketch.js
+++ chess/sketch.js
@@ -5,28 +5,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 let boxSize;
-let filledwithblack;
-
-
+let isFilledWithBlack;
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(600, 600);
   boxSize = width/8;
-  filledwithblack = false;
+  isFilledWithBlack = false;
 }
 
 function draw() {
-  for (let i = 0; i < 8; i++){
-    for (let j = 0; j < 8; j++){
-      if (filledwithblack) {
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      if (isFilledWithBlack) {
         fill(0);
       }
       else {
         fill(255);
       }
-      rect(i * boxSize,j * boxSize ,boxSize,boxSize);
-      filledwithblack = !filledwithblack;
+      rect(i * boxSize, j * boxSize, boxSize, boxSize);
+      isFilledWithBlack = !isFilledWithBlack;
     }
-    filledwithblack = filledwithblack;
+    isFilledWithBlack = !isFilledWithBlack;
   }
+
 }
