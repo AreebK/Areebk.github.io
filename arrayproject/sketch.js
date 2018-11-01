f23d0dec01663c0ecc09252a8b3d2bd53114a1d9
AreebK <43014295+AreebK@users.noreply.github.com>
2018-11-01 14:14:14 -0600

    Created start screen / troubleshooting with key typed function

--- arrayproject/sketch.js
+++ arrayproject/sketch.js
@@ -12,8 +12,10 @@ let cellSize;
 let brick;
 let stone;
 let diamond;
-let state;
+let state = 0;
 let start;
+let winW;
+let winH;
 
 
 function preload(){
@@ -25,12 +27,19 @@ function preload(){
 }
 
 function setup() {
-  if (windowWidth > windowHeight){
+  if (state === 0){
+    createCanvas(windowWidth, windowHeight);
+  }
+  else if (state === 1 && windowWidth > windowHeight){
     createCanvas(windowHeight, windowHeight);
+    winH = windowHeight;
   }
-  else if (windowHeight > windowWidth) {
+  else if (state === 1 && windowHeight > windowWidth) {
     createCanvas(windowWidth, windowWidth);
+    winW = windowWidth;
   }
+
+
   rows = maze[0].length;
   cols = maze[0].length;
   cellSize = width / cols;
@@ -45,30 +54,54 @@ function cleanUpMaze() {
 
 function draw() {
   background(255);
-  drawMaze();
+  startScreen();  //creates a start screen in which you will have to click to make the state change to create the Maze
+  drawMaze();   // only created once the start screen is clicked the state has successfully changed
+  keyTyped();
 }
 
+function startScreen() {
+  textAlign(CENTER);
+  if (state === 0) {
+    background(0);
+    textSize(500);
+    fill(0, 255, 0);
+    text("START", width/ 2, height / 2 - 50);
+    fill(255);
+    textSize(200);
+    text("To begin press S", width/2, height /2 + 200);
 
+  }
+}
+
+function keyTyped(){
+  if (key === "s" || key === "S"){
+    state === 1;
+    return state;
+  }
+
+}
 
 function drawMaze() {
-  for (let i = 0; i < cols; i++){
-    for (let j = 0; j < rows; j++){
-      if (maze[j][i] === "0"){
-        stroke(0);
-        fill(255);
-        image(stone,i * cellSize,j* cellSize, cellSize, cellSize);
-      }
-      else if (maze[j][i] === "1") {
-        stroke(0);
-        fill(0);
-        image(brick,i * cellSize,j* cellSize, cellSize, cellSize);
-      }
-      else if (maze[j][i] === "2") {
-        stroke(0);
-        fill(0);
-        image(diamond,i * cellSize,j* cellSize, cellSize, cellSize);
+  if (state === 1){
+    for (let i = 0; i < cols; i++){
+      for (let j = 0; j < rows; j++){
+        if (maze[j][i] === "0"){
+          stroke(0);
+          fill(255);
+          image(stone,i * cellSize,j* cellSize, cellSize, cellSize);
+        }
+        else if (maze[j][i] === "1") {
+          stroke(0);
+          fill(0);
+          image(brick,i * cellSize,j* cellSize, cellSize, cellSize);
+        }
+        else if (maze[j][i] === "2") {
+          stroke(0);
+          fill(0);
+          image(diamond,i * cellSize,j* cellSize, cellSize, cellSize);
+        }
       }
     }
+    return drawMaze;
   }
-  return drawMaze;
 }
