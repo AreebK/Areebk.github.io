b0477b8ee24aba7753a59d650c872bd4b2a76e63
AreebK <43014295+AreebK@users.noreply.github.com>
2018-11-28 13:44:57 -0600

    Work On back Button

--- p5-2/sketch.js
+++ p5-2/sketch.js
@@ -8,22 +8,37 @@ let state;
 let canvasWidth, canvasHeight;
 let hoverPlay = false;
 let hoverControls = false;
+let hoverBack = false;
+let hoverBackControl = false;
 let fillStart, fillControl;
+let mainMenuScreen, controlText;
+
+function preload() {
+  mainMenuScreen = loadImage("assets/background.png");
+  controlText = loadImage("assets/controlRules.png");
+}
 
 function setup() {
   createCanvas(1600, 790);
   state = "MainMenu";
   canvasWidth = 1600;
   canvasHeight = 790;
+  if (state === "MainMenu") {
+    image(mainMenuScreen, 0, 0, 1600, 790);
+  }
 }
 
 function draw() {
   mainMenu();
-  hoverPlay = collidePointRect(mouseX, mouseY, 400, 145, 1600/2 - 100, 790/3 - 33.34);
-  hoverControls = collidePointRect(mouseX, mouseY, 575, 465.3334, 350, 175);
-  // fillStart = fill(112, 25, 112);
-  // fillControl = fill(112, 25, 112);
+  hoverPlay = collidePointRect(mouseX, mouseY, 400, 220, 1600/2 - 100, 790/4 - 33.34);
+  hoverControls = collidePointRect(mouseX, mouseY, 575, 500.3334, 320, 175/1.5 );
+  if (state === "gameStart") {
+    background(255, 0, 0);
+  }
+  else if (state === "controlSettings") {
+    image(controlText, 0, 0, 1600, 790);
 
+  }
 }
 
 function mainMenu() {
@@ -31,17 +46,14 @@ function mainMenu() {
     push();
     let fillStart = controlStart();
     fill(fillStart);
-    rect(400, 145, 1600/2 - 100, 790/3 - 33.34);
+    rect(400, 220, 1600/2 - 100, 790/4 - 33.34);
     pop();
 
     push();
     let fillControl = controlFill();
     fill(fillControl);
-    rect(575, 465.3334, 350, 175);
+    rect(575, 500.3334, 350, 175/2);
     pop();
-
-
-
   }
 }
 
@@ -49,7 +61,7 @@ function controlStart() {
 
   if (state === "MainMenu"){
     if(hoverPlay){ // This means if mouse x and y are in the certian cordinates then it will return triggering the if staement
-      fillStart = color(25, 25, 112);
+      fillStart = color(255);
     }
     else{
       fillStart = color(0,255,255);
@@ -58,16 +70,32 @@ function controlStart() {
   }
 }
 
+function backButton() {
+  
+}
+
 
 function controlFill() {
   let fillControl;
   if (state === "MainMenu"){
     if(hoverControls){ // This means if mouse x and y are in the certian cordinates then it will return triggering the if staement
-      fillControl = color(25, 25, 112);
+      fillControl = color(255);
     }
     else{
       fillControl = color(0, 255, 255);
     }
     return fillControl;
+  }
+}
+
+
+function mousePressed() {
+  if (state === "MainMenu"){
+    if (hoverPlay) {
+      state = "gameStart";
+    }
+    else if (hoverControls) {
+      state = "controlSettings";
+    }
   }
 }
