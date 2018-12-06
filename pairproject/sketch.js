bdbca3aee26c2c1e5903dabb385ed7c1503db251
AreebK <43014295+AreebK@users.noreply.github.com>
2018-12-06 13:39:17 -0600

    Background music (broken)

--- pairproject/sketch.js
+++ pairproject/sketch.js
@@ -1,23 +1,26 @@
-// Project Title
-// Your Name
-// Date
+// Asteroid Destoyer
+// Areeb Khan, Tyndall Johnston, Edam ____
+// Due Date 12/5/2018 (We had an extension)
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let state, jokeSound;
+
+
+let state, jokeSound, backgroundNoise;
 let canvasWidth, canvasHeight;
-let hoverPlay = false;
-let hoverControls = false;
-let hoverBackControl = false;
-let hoverSecret = false;
-let hoverYes = false;
-let hoverNo = false;
+let hoverPlay = false; // 2D Collide function that defaults the play button as false
+let hoverControls = false; // 2D Collide function that defaults the Control button as false
+let hoverBackControl = false; // 2D Collide function that defaults the BackControl button as false
+let hoverSecret = false; // 2D Collide function that defaults the Secret Menu button as false
+let hoverYes = false; // 2D Collide function that defaults the Yes button as false
+let hoverNo = false; // 2D Collide function that defaults the No button as false
 let fillStart, fillControl, fillSecret, fillYes, fillNo;
 let mainMenuScreen, controlText;
 let backButtonControls;
-let playimg, backimg, controlimg, clicksound, secretimg, yesimg, noimg, jokeClick, jokeStart, jokeback, jokeShoot, jokeHit, jokedie;
+let playimg, backimg, controlimg, clicksound, secretimg, yesimg, noimg, jokeClick, jokeStart, jokeback, jokeshootsound, jokehitsound, jokediesound;
 
 function preload() {
+  //Preloading every file from the png, to mp3 files
   mainMenuScreen = loadImage("assets/background.png");
   controlText = loadImage("assets/controlRules.png");
   playimg = loadImage("assets/play.png");
@@ -30,54 +33,50 @@ function preload() {
   jokeClick = loadSound("assets/jokeClick.mp3");
   jokeStart = loadSound("assets/Jokesoundstart.mp3");
   jokeback = loadSound("assets/jokeBackground.mp3");
-  // jokeShoot = loadSound("assets/jokeShoot.mp3");
-  // jokeHit = loadSound("assets/jokehit.mp3");
-  // jokedie = loadImage("assets/jokedie.mp3");
+  jokeshootsound = loadSound("assets/jokeShoot.mp3");
+  jokehitsound = loadSound("assets/jokehit.mp3");
+  jokediesound = loadSound("assets/jokedie.mp3");
 }
 
 function setup() {
-  createCanvas(1600, 790);
-  state = "MainMenu";
+  createCanvas(1600, 790); // Custom Canvas size to fit perfectly on School monitor
+  state = "MainMenu"; // The State function defaults to the MainMenu
   canvasWidth = 1600;
   canvasHeight = 790;
-  jokeSound = "notActive";
+  jokeSound = "notActive"; // Defaults the Joke Sound Affects to "notActive" and can be accesed through EasterEgg from the Game
   clicksound.setVolume(0.1);
   jokeClick.setVolume(1.0);
   jokeStart.setVolume(0.4);
-  jokeback.setVolume(0.5);
-  // jokeShoot.setVolume(0.5);
-  // jokeHit.setVolume(0.5);
-  // jokedie.setVolume(1.0);
-  // if (jokeSound === "notActive"){
-  //   //hi
-  // }
-  // if (jokeSound === "Active"){
-  //   jokeback.loop();
-  // }
+  jokeback.setVolume(0.23);
+  jokeshootsound.setVolume(0.5);
+  jokehitsound.setVolume(0.5);
+  jokediesound.setVolume(0.5);
+  // jokeback.loop(); // The Background music ("As of now cannot function unless uncommented")
 }
 
+
 function draw() {
-  if (state === "MainMenu") {
+  if (state === "MainMenu") { // When the MainMenu State is true this image is popping up
     image(mainMenuScreen, 0, 0, 1600, 790);
   }
-  else if (state === "controlSettings") {
+  else if (state === "controlSettings") { // When the Control Settings state is true displays the image showing the controls for the game
     image(controlText, 0, 0, 1600, 790);
   }
-  else if (state === "secretMenu") {
+  else if (state === "secretMenu") { // When the Secret Menu setting is true, it displays the image letting you chose whether or not you want our "Joke Sound Affects"
     image(secretimg, 0, 0, 1600, 790);
+  }
+  else if (state === "gameStart") { // When the Game Start setting is true, it displays the game letting you play around
+    background(255, 0, 0);
+
   }
 
   mainMenu();
-  hoverPlay = collidePointRect(mouseX, mouseY, 400, 220, 1600/2 - 100, 790/4 - 33.34);
-  hoverControls = collidePointRect(mouseX, mouseY, 575, 500.3334, 350, 175/2);
-  hoverBackControl = collidePointRect(mouseX, mouseY, 100, 665, 175, 100);
-  hoverSecret = collidePointRect(mouseX, mouseY, 0, 0, 10, 10);
-  hoverYes = collidePointRect(mouseX, mouseY, 400, 220, 1600/2 - 100, 790/4 - 33.34);
-  hoverNo = collidePointRect(mouseX, mouseY, 575, 500.3334, 1600/2 - 100, 790/4 - 33.34);
-
-  if (state === "gameStart") {
-    background(255, 0, 0);
-  }
+  hoverPlay = collidePointRect(mouseX, mouseY, 400, 220, 1600/2 - 100, 790/4 - 33.34); //
+  hoverControls = collidePointRect(mouseX, mouseY, 575, 500.3334, 350, 175/2); //
+  hoverBackControl = collidePointRect(mouseX, mouseY, 100, 665, 175, 100); //
+  hoverSecret = collidePointRect(mouseX, mouseY, 0, 0, 10, 10); //
+  hoverYes = collidePointRect(mouseX, mouseY, 400, 220, 1600/2 - 100, 790/4 - 33.34); //
+  hoverNo = collidePointRect(mouseX, mouseY, 575, 500.3334, 1600/2 - 100, 790/4 - 33.34); //
 }
 
 function mainMenu() {
